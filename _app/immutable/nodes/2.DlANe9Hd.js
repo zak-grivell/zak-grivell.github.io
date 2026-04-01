f109d753dfd4ba782ebfb1140b66384168e0579b
zak grivell <zak@grivell.uk>
2026-04-01 23:01:30 +0100

    Updates

---
+++ _app/immutable/nodes/2.DlANe9Hd.js
@@ -0,0 +1,1 @@
+import{a as o,f as a}from"../chunks/mi4kV0sK.js";import{n as r}from"../chunks/CCoE3isk.js";var m=a('<h1>Welcome to SvelteKit</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>',1);function s(t){var e=m();r(2),o(t,e)}export{s as component};
